3550dee3b66a84e6aac93324ba867d6e3c77b57f
mariana <mavila@oswego.edu>
2023-10-13 00:10:54 -0400

    project

--- captsoneproject/src/PollenProvider.js
+++ captsoneproject/src/PollenProvider.js
@@ -9,10 +9,16 @@ export default function PollenProvider({children}){
     const [treePollen, setTreePollen] = useState( '0');
     const [grassPollen, setGrassPollen] = useState( '0');
     const [weedPollen, setWeedPollen] = useState( '0');
-    
+    //plants
+    const [graminales,setGraminales]= useState('0');
+    const [mugwort, setMugwort]= useState('0');
+    const [hazel,setHazel]=useState('0');
+    const [oak,setOak]= useState('0');
+    const [alder,setalder]= useState('0'); 
+
     return(
        
-            <PollenContext.Provider value={{ lat, setLat , zcode, setZcode,long, setLong, treePollen, setTreePollen,grassPollen,setGrassPollen,weedPollen,setWeedPollen}}>
+            <PollenContext.Provider value={{ lat, setLat , zcode, setZcode,long, setLong, treePollen, setTreePollen,grassPollen,setGrassPollen,weedPollen,setWeedPollen,graminales,setGraminales,mugwort,setMugwort,hazel,setHazel,oak,setOak,alder,setalder}}>
               {children}
             </PollenContext.Provider>
     );
